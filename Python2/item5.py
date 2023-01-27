9ca3d0e23516387a3ca0a25ebdd2c1999056e30b
neqq <88395240+supitchapsr@users.noreply.github.com>
2023-01-27 10:04:36 +0700

    1st commit

---
+++ Python2/item5.py
@@ -0,0 +1,17 @@
+# ตึกลึกลับแห่งหนึ่งเมื่อเดินไปข้างหลังจะมีคนบอกรหัสลับมาจงสร้างฟังชั่นคำนวณรหัส
+# โดยรหัสจะประกอบไปด้วย english word that have repeat character
+# เช่น bon("ball") = 48 หรือ bon("aah") = 4
+
+# Enter secret code : ball
+# 48
+
+# Enter secret code : ascii
+# 36
+
+
+def bon(w):
+    return
+
+
+secretCode = input("Enter secret code : ")
+print(bon(secretCode))
