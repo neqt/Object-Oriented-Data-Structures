7d12d9ebba46ffce6ade29d3e15ca935fc24da86
neqq <88395240+supitchapsr@users.noreply.github.com>
2023-01-28 21:54:43 +0700

    Fixed bug

--- Python2/item3.py
+++ Python2/item3.py
@@ -8,44 +8,9 @@
 # (0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
 
 # *** New Range ***
-# Enter Input : 3.2 6.32
-# (3.2, 4.2, 5.2, 6.2)
-
-# *** New Range ***
 # Enter Input : 0.112 6.45 1.35
 # (0.112, 1.462, 2.812, 4.162, 5.512)
 
-# *** New Range ***
-# Enter Input : 1 7 1.2
-# (1.0, 2.2, 3.4, 4.6, 5.8)
-
-# *** New Range ***
-# Enter Input : 1.2 5.9 0.45
-# (1.2, 1.65, 2.1, 2.55, 3.0, 3.45, 3.9, 4.35, 4.8, 5.25, 5.7)
-
-# def new_range(a, b=None, c=None):
-#     temp = []
-
-#     if a != None and b == None and c == None:
-#         start = 0
-#         end = a
-#         step = 1
-#     if a != None and b != None and c == None:
-#         start = a
-#         end = b
-#         step = 1
-#     if a != None and b != None and c != None:
-#         start = a
-#         end = b
-#         step = c
-
-#     while (start < end):
-#         temp.append(start)
-#         print(str(start)+" + "+str(step)+" = "+str(start+step)+"\n")
-#         # start = sum([start, step])
-#         start += step
-
-#     return print(tuple(temp))
 
 def new_range(*args):
     if len(args) == 1:
@@ -60,15 +25,14 @@ def new_range(*args):
         start = args[0]
         end = args[1]
         step = args[2]
-    else:
-        return "Invalid number of arguments"
 
-    lst = [start]
-    while start + step < end:
+    temp = []
+    while (start < end):
+        temp.append(start)
         start += step
-        lst.append(start)
+    ans = [round(num, 3) for num in temp]
 
-    return lst
+    return tuple([float(i) for i in ans])
 
 
 inp = [float(i) for i in input('*** New Range ***\nEnter Input : ').split()]
