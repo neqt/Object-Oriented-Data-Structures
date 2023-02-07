818849570782735fce45a95b4efc31efbe8000b3
neqq <88395240+supitchapsr@users.noreply.github.com>
2023-02-08 01:10:53 +0700

    Fixed bug

--- Python2/item5.py
+++ Python2/item5.py
@@ -1,16 +1,22 @@
 # ตึกลึกลับแห่งหนึ่งเมื่อเดินไปข้างหลังจะมีคนบอกรหัสลับมาจงสร้างฟังชั่นคำนวณรหัส
-# โดยรหัสจะประกอบไปด้วย english word that have repeat character
+# โดยรหัสจะประกอบไปด้วย english w that have repeat character
 # เช่น bon("ball") = 48 หรือ bon("aah") = 4
-
-# Enter secret code : ball
-# 48
-
-# Enter secret code : ascii
-# 36
 
 
 def bon(w):
-    return
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    w = w.lower()
+    code = 0
+    i = 0
+
+    while i < len(w):
+        j = i + 1
+        while j < len(w) and w[i] == w[j]:
+            j += 1
+            code = 4 * (alphabet.index(w[i]) + 1)
+        i = j
+
+    return code
 
 
 secretCode = input("Enter secret code : ")
